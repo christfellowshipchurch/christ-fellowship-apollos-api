88254ebddbabd4e106a00bf1692cb90ba6accb48
Caleb Panza <caleb.panza@icloud.org>
2019-03-19 21:20:26 -0400

    Added Defined Value data type

---
+++ src/data/defined-value/__tests__/data-source.tests.js
@@ -0,0 +1,68 @@
+import ApollosConfig from '@apollosproject/config';
+import DefinedValue from '../data-source';
+import { buildGetMock } from '../../test-utils';
+
+ApollosConfig.loadJs({
+    ROCK: {
+        API_URL: 'https://apollosrock.newspring.cc/api',
+        API_TOKEN: 'some-rock-token',
+        IMAGE_URL: 'https://apollosrock.newspring.cc/GetImage.ashx',
+    },
+});
+
+describe('Defined Value', () => {
+    // Parse Identifier parameter
+    it('parses identifer as a guid', () => {
+        const dataSource = new DefinedValue();
+        const id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+        const identifierType = dataSource.getIdentifierType(id);
+
+        expect(identifierType).toEqual({ type: 'guid', value: id, query: `Guid eq (guid'${id}')` });
+    });
+
+    it('parses identifer as an integer', () => {
+        const dataSource = new DefinedValue();
+        const id = '999';
+        const identifierType = dataSource.getIdentifierType(id);
+
+        expect(identifierType).toEqual({ type: 'int', value: id, query: `Id eq ${id}` });
+    });
+
+    it('parses identifer as a custom identifer', () => {
+        const dataSource = new DefinedValue();
+        const id = 'some-custom-identifer';
+        const identifierType = dataSource.getIdentifierType(id);
+    });
+
+
+    // Run the getValueBYBlablablablab
+    it('gets a defined value from a valid guid or integer id', async () => {
+        const dataSource = new DefinedValue();
+        const id = '999'
+
+        dataSource.get = jest.fn();
+
+        const result = await dataSource.getDefinedValueByIdentifier(id);
+
+        expect(dataSource.get.mocks).toMatchSnapshot();
+        const id = '999'
+
+        dataSource.get = jest.fn();
+
+        const result = await dataSource.getDefinedValueByIdentifier(id);
+
+        expect(dataSource.get.mocks).toMatchSnapshot();
+    });
+
+    it('returns null if no valid id or guid is passed', () => {
+        const dataSource = new DefinedValue();
+        const id = 'some-random-identifier'
+
+        dataSource.get = jest.fn();
+
+        const result = await dataSource.getDefinedValueByIdentifier(id);
+
+        expect(dataSource.get.mocks).toMatchSnapshot();
+        expect(result).toEqual(null);
+    });
+});
