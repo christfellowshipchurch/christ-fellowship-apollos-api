b605ce0223a73dd8a6cf5c9cf0661b827a6b1d8a
Caleb Panza <caleb.panza@icloud.org>
2019-03-22 17:00:55 -0400

    Updated API to return full video URI's

--- src/data/website-content-item/resolver.js
+++ src/data/website-content-item/resolver.js
@@ -1,12 +1,40 @@
-import { ContentItem } from '@apollosproject/data-connector-rock';
+import { ContentItem, Utils } from '@apollosproject/data-connector-rock';
 import { schemaMerge } from '@apollosproject/server-core';
+import ApollosConfig from '@apollosproject/config';
 import {
     get
 } from 'lodash';
 
+const createVideoUrlFromGuid = (uri) =>
+    uri.split('-').length === 5
+        ? `${ApollosConfig.ROCK.FILE_URL}?guid=${uri}`
+        : Utils.enforceProtocol(uri);
+
 const resolver = {
     WebsiteContentItem: {
         ...ContentItem.resolver.ContentItem,
+        title: ({ title, attributeValues }, args, context) => {
+            const titleOverride = get(attributeValues, 'titleOverride.value', '');
+
+            return titleOverride === ''
+                ? title
+                : titleOverride;
+        },
+        videos: (root, args, { dataSources: { ContentItem } }) => {
+            const videos = ContentItem.getVideos(root);
+
+            return videos.map((video) => {
+                video.sources = video.sources.map((sources) => {
+                    sources.uri = createVideoUrlFromGuid(sources.uri)
+                    return sources;
+                });
+
+                return video;
+            });
+        },
+
+        // NOTE : removes the sanitizeHtml() from the content, so Graph passes back all of the HTML that Rock is returning
+        // htmlContent: ({ content }) => content,
         contentLayout: async ({ attributeValues }, args, context) => {
             const definedValueGuid = get(attributeValues, 'contentLayout.value', '');
             const definedValue = await context.dataSources.DefinedValue.getDefinedValueByIdentifier(definedValueGuid);
