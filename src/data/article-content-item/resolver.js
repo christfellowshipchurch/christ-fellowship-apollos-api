73dcd6fe5be7bf71626433e4318fa2ad5c40bf00
Caleb Panza <caleb.panza@icloud.org>
2019-09-28 13:45:13 -0400

    Added article endpoints

--- src/data/article-content-item/resolver.js
+++ src/data/article-content-item/resolver.js
@@ -2,10 +2,11 @@ import { ContentItem, Utils } from '@apollosproject/data-connector-rock'
 import { resolverMerge } from '@apollosproject/server-core'
 import ApollosConfig from '@apollosproject/config'
 import {
-    get
+    get, has
 } from 'lodash'
 import { parseRockKeyValuePairs, parseHexCode } from '../utils'
 
+const { createImageUrlFromGuid } = Utils
 const createVideoUrlFromGuid = (uri) =>
     uri.split('-').length === 5
         ? `${ApollosConfig.ROCK.FILE_URL}?guid=${uri}`
@@ -13,7 +14,9 @@ const createVideoUrlFromGuid = (uri) =>
 
 const resolver = {
     Query: {
-        getArticleContentItemByTitle: async (root, { title }, context) =>
+        getArticles: (root, { title }, context) =>
+            context.dataSources.ArticleContentItem.getArticles(),
+        getArticleByTitle: async (root, { title }, context) =>
             await context.dataSources.ArticleContentItem.getArticleContentItemByTitle(title),
     },
     ArticleContentItem: {
@@ -26,7 +29,24 @@ const resolver = {
                 : titleOverride;
         },
         htmlContent: ({ content }) => content,
-        subtitle: ({ attributeValues }) => get(attributeValues, 'subtitle.value', null),
+        summary: ({ attributeValues }) => get(attributeValues, 'subtitle.value', null),
+        author: async ({ attributeValues }, args, { dataSources }) => {
+            if (has(attributeValues, 'author.value')) {
+                const { id } = await dataSources.Person.getFromAliasId(attributeValues.author.value)
+
+                const person = await dataSources.Person.getFromId(id)
+
+                return {
+                    ...person,
+                    photo: {
+                        url: createImageUrlFromGuid(get(person, 'photo.guid', ''))
+                    }
+                }
+            }
+
+            return null
+        },
+        readTime: ({ attributeValues }) => get(attributeValues, 'estimatedReadTime.value', null),
     }
 }
 
