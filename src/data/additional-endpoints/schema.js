1ac0f8f76a76b23cb6263e87aba7f965704ea43c
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-03-11 12:12:02 -0400

    More tab links (#55)
    * JSON file with More Link tables
    
    * Added resolvers for moreLinks
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

--- src/data/additional-endpoints/schema.js
+++ src/data/additional-endpoints/schema.js
@@ -1,7 +1,20 @@
 import gql from 'graphql-tag'
 
 export default gql`
+  type AppLink {
+    name: String!
+    uri: String
+    icon: String
+    openInApp: Boolean
+  }  
+
+  type AppLinks {
+    name: String!
+    links: [AppLink]
+  }
+
   extend type Query { 
     privacyPolicyUrl: String
+    moreLinks: [AppLinks]
   }
 `
