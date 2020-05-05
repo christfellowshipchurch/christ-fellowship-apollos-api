ce3a5e4bb2784eaf70febb1e132f47770d81a33d
Caleb Panza <caleb.panza@icloud.org>
2020-05-05 09:57:25 -0400

    Redis cache updated to 60 minutes

--- src/data/redis-cache/data-source.js
+++ src/data/redis-cache/data-source.js
@@ -1,5 +1,5 @@
 import * as RedisCache from '@apollosproject/data-connector-redis-cache';
 
 export default class Cache extends RedisCache.dataSource {
-  DEFAULT_TIMEOUT = 60 * 5
+  DEFAULT_TIMEOUT = 60 * 60
 }
