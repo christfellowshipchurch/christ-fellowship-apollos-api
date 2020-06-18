8f99b994f4abe2a4103ea3dbc8a350f2e93fa4a5
Caleb Panza <caleb.panza@icloud.org>
2020-06-18 12:21:11 -0400

    Clean and fix redis caching issues
    localized jobs.js and added a distinguishing key for algolia jobs

--- src/data/search/index.js
+++ src/data/search/index.js
@@ -2,6 +2,6 @@ import * as CoreSearch from '@apollosproject/data-connector-algolia-search';
 
 export const resolver = CoreSearch.resolver
 export const schema = CoreSearch.schema
-export const jobs = CoreSearch.jobs
 
 export { default as dataSource } from './data-source';
+export { default as jobs } from './jobs';
