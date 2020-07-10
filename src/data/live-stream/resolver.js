b4ad287f9bc56eea6528121d3106934dc7bbe6b7
Caleb Panza <caleb.panza@icloud.org>
2020-07-10 12:08:33 -0400

    Added live stream endpoints for FloatLeft

--- src/data/live-stream/resolver.js
+++ src/data/live-stream/resolver.js
@@ -1,8 +1,25 @@
 import { resolverMerge } from '@apollosproject/server-core'
 import * as coreLiveStream from '@apollosproject/data-connector-church-online'
+import moment from 'moment'
 
 const resolver = {
-  LiveStream: coreLiveStream.resolver.LiveStream
+  LiveStream: coreLiveStream.resolver.LiveStream,
+  Query: {
+    floatLeftLiveStream: (root, args, { dataSources }) => ({
+      isLive: true,
+      eventStartTime: moment().add(15, "minutes").utc().toISOString(),
+      title: "Always Live",
+      contentItem: dataSources.ContentItem.getFromId(7565),
+      media: {
+        sources: [
+          {
+            uri: "https://link.theplatform.com/s/IfSiAC/media/h9hnjqraubSs/file.m3u8?metafile=false&formats=m3u&auto=true"
+          }
+        ]
+      },
+    }),
+    floatLeftEmptyLiveStream: (root, args, { dataSources }) => null
+  }
 }
 
 export default resolverMerge(resolver, coreLiveStream)
