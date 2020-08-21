cd9d6848e276e0f1c5b4a3e791e30f065f25ce72
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-08-21 10:03:14 -0400

    [CFDP-869] Added feature flag key NOTIFICATION_CENTER (#116)
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

--- src/data/redis-cache/data-source.js
+++ src/data/redis-cache/data-source.js
@@ -1,7 +1,5 @@
 import * as RedisCache from '@apollosproject/data-connector-redis-cache';
 
-console.log(RedisCache.dataSource);
-
 export default class Cache extends RedisCache.dataSource {
   // 1 hour cache
   DEFAULT_TIMEOUT = 60 * 60
