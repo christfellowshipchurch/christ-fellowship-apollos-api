59ad08ad2441f99e467ca0d924601261183c8047
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-08-24 15:39:58 -0400

    [CFDP-870] Notification center mvp (#118)
    * Added Message schema
    
    * Minor resolver clean up
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

---
+++ src/data/message/resolver.js
@@ -0,0 +1,40 @@
+import {
+  createGlobalId,
+  withEdgePagination,
+} from '@apollosproject/server-core';
+import ApollosConfig from '@apollosproject/config'
+import { get } from 'lodash'
+import moment from 'moment-timezone'
+
+const { ROCK } = ApollosConfig
+
+const resolver = {
+  Message: {
+    id: ({ id }, args, context, { parentType }) =>
+      createGlobalId(id, parentType.name),
+    title: ({ attributeValues }, args, context) =>
+      get(attributeValues, 'title.value', ''),
+    subtitle: ({ attributeValues }, args, context) =>
+      get(attributeValues, 'subtitle.value', ''),
+    body: ({ attributeValues }, args, context) =>
+      get(attributeValues, 'body.value', ''),
+    date: ({ attributeValues }, args, context) =>
+      moment
+        .tz(get(attributeValues, 'date.value', ''), ROCK.TIMEZONE)
+        .utc()
+        .format(),
+  },
+  MessagesConnection: {
+    totalCount: ({ getTotalCount }) => getTotalCount(),
+    pageInfo: withEdgePagination,
+  },
+  Query: {
+    notificationCenter: (root, args, { dataSources }) =>
+      dataSources.Message.paginate({
+        cursor: dataSources.Message.getByNotificationCenter(),
+        args,
+      }),
+  },
+}
+
+export default resolver
