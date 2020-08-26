ff54e655805998061e037902de49f1f3246c78f7
Caleb Panza <caleb.panza@icloud.org>
2020-08-26 12:19:19 -0400

    [CFDP-876] Add paginated endpoint for my prayers

--- src/data/prayer-request/resolver.js
+++ src/data/prayer-request/resolver.js
@@ -1,7 +1,18 @@
 import { PrayerRequest as corePrayerRequest } from '@apollosproject/data-connector-rock'
-import { resolverMerge } from '@apollosproject/server-core'
+import { resolverMerge, withEdgePagination } from '@apollosproject/server-core'
 
 const resolver = {
+    PrayerRequestsConnection: {
+        totalCount: ({ getTotalCount }) => getTotalCount(),
+        pageInfo: withEdgePagination,
+    },
+    Query: {
+        currentUserPrayerRequests: async (root, args, { dataSources }) =>
+            dataSources.PrayerRequest.paginate({
+                cursor: await dataSources.PrayerRequest.byCurrentUser(),
+                args,
+            }),
+    }
 }
 
 export default resolverMerge(resolver, corePrayerRequest)
--- src/data/prayer-request/schema.js
+++ src/data/prayer-request/schema.js
@@ -1,3 +1,28 @@
+import { gql } from 'apollo-server'
 import { prayerSchema } from '@apollosproject/data-schema'
 
-export default prayerSchema
+export default gql`
+    ${prayerSchema}
+
+    input PrayerRequestsConnectionInput {
+        first: Int
+        after: String
+    }
+
+    type PrayerRequestsConnection {
+        edges: [PrayerRequestsConnectionEdge]
+        totalCount: Int
+        pageInfo: PaginationInfo
+    }
+
+    type PrayerRequestsConnectionEdge {
+        node: PrayerRequest
+        cursor: String
+    }
+
+    extend type Query {
+        currentUserPrayerRequests(
+            first: Int
+        ): PrayerRequestsConnection
+    }
+`
