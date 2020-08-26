dc70070a1148e71e3f2ece49175757993e03027b
Ryan Davidson <ryan@rkdavidson.com>
2020-08-26 09:04:43 -0400

    Remove console logs

--- src/data/stream-chat/data-source.js
+++ src/data/stream-chat/data-source.js
@@ -9,13 +9,12 @@ const { CHAT_SECRET, CHAT_API_KEY, CHAT_APP_ID } = STREAM;
 // Define singleton instance of StreamChatClient
 let chatClient;
 
-if (CHAT_SECRET && CHAT_API_KEY) {
+if (CHAT_SECRET && CHAT_API_KEY && !chatClient) {
   chatClient = new StreamChatClient(
     CHAT_API_KEY,
     CHAT_SECRET,
     { region: "us-east-1" }
   );
-  console.log('[rkd] ✅ Created chat client');
 } else {
   console.warn(
     "You are using the Stream Chat dataSource without Stream credentials. To avoid issues, add Stream Chat credentials to your config.yml or remove the Stream Chat dataSource"
@@ -24,13 +23,9 @@ if (CHAT_SECRET && CHAT_API_KEY) {
 
 export default class StreamChat extends RESTDataSource {
   generateUserToken = async (currentPerson) => {
-    const globalId = createGlobalId(currentPerson.id, "AuthenticatedUser"); // -> "AuthenticatedUser:abc123de56789f"
+    const globalId = createGlobalId(currentPerson.id, "AuthenticatedUser");
     const userId = globalId.split(":")[1];
 
-    console.log('[rkd] creating token for userId:', userId);
-    const token = await chatClient.createToken(userId);
-    console.log('[rkd] token:', token);
-
-    return token;
+    return await chatClient.createToken(userId);
   };
 }
