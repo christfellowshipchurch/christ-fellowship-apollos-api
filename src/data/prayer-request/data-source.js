e46eb43141bd3e7b3a4308db45645581ff8e6720
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-08-27 17:01:24 -0400

    [CFDP-878] My prayers (#121)
    * [CFDP-876] Add paginated endpoint for my prayers
    
    * [CFDP-878] Adds requestedDate to PrayerRequest schema
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

--- src/data/prayer-request/data-source.js
+++ src/data/prayer-request/data-source.js
@@ -2,8 +2,9 @@ import {
     PrayerRequest as corePrayerRequest,
 } from '@apollosproject/data-connector-rock'
 import ApollosConfig from '@apollosproject/config'
+import moment from 'moment-timezone'
 
-const { ROCK_MAPPINGS } = ApollosConfig
+const { ROCK, ROCK_MAPPINGS } = ApollosConfig
 
 export default class PrayerRequest extends corePrayerRequest.dataSource {
 
@@ -14,5 +15,24 @@ export default class PrayerRequest extends corePrayerRequest.dataSource {
 
         return requestBuilder
             .andFilter(`CategoryId eq ${ROCK_MAPPINGS.GENERAL_PRAYER_CATEGORY_ID}`)
+    }
+
+    async byCurrentUser() {
+        const {
+            dataSources: { Auth },
+        } = this.context;
+
+        const { primaryAliasId } = await Auth.getCurrentPerson();
+
+        return this.request()
+            .filter(`RequestedByPersonAliasId eq ${primaryAliasId}`) // only show your own prayers
+            .andFilter(`IsActive eq true`) // prayers can be marked as "in-active" in Rock
+            .andFilter(`IsApproved eq true`) // prayers can be moderated in Rock
+            .andFilter('IsPublic eq true') // prayers can be set to private in Rock
+            .andFilter(`Answer eq null or Answer eq ''`) // prayers that aren't answered
+            .sort([
+                { field: 'PrayerCount', direction: 'asc' }, // # of times prayed, ascending
+                { field: 'EnteredDateTime', direction: 'asc' }, // oldest prayer first
+            ]);
     }
 }
--- src/data/prayer-request/resolver.js
+++ src/data/prayer-request/resolver.js
@@ -1,7 +1,29 @@
 import { PrayerRequest as corePrayerRequest } from '@apollosproject/data-connector-rock'
-import { resolverMerge } from '@apollosproject/server-core'
+import { resolverMerge, withEdgePagination } from '@apollosproject/server-core'
+import ApollosConfig from '@apollosproject/config'
+import moment from 'moment-timezone'
+
+const { ROCK } = ApollosConfig
 
 const resolver = {
+    PrayerRequest: {
+        requestedDate: ({ enteredDateTime }) =>
+            moment(enteredDateTime)
+                .tz(ROCK.TIMEZONE)
+                .utc()
+                .format()
+    },
+    PrayerRequestsConnection: {
+        totalCount: ({ getTotalCount }) => getTotalCount(),
+        pageInfo: withEdgePagination,
+    },
+    Query: {
+        currentUserPrayerRequests: async (root, args, { dataSources }) =>
+            dataSources.PrayerRequest.paginate({
+                cursor: await dataSources.PrayerRequest.byCurrentUser(),
+                args,
+            }),
+    }
 }
 
 export default resolverMerge(resolver, corePrayerRequest)
