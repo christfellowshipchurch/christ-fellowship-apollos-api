353fa403cecc1827b319cecc44d656f63f6082ea
Jordan Wade <jjwade31@gmail.com>
2020-11-02 13:21:17 -0600

    Add LiveStreamAction to features schema

--- src/data/features/schema.js
+++ src/data/features/schema.js
@@ -1,57 +1,66 @@
-import { featuresSchema } from '@apollosproject/data-schema'
-import gql from 'graphql-tag'
+import { featuresSchema } from '@apollosproject/data-schema';
+import gql from 'graphql-tag';
 
 export default gql`
-    ${featuresSchema}
+  ${featuresSchema}
 
-    extend enum ACTION_FEATURE_ACTION {
-        VIEW_CHILDREN
-        READ_GLOBAL_CONTENT
-        READ_PRAYER
-        READ_GROUP
-    }
+  extend enum ACTION_FEATURE_ACTION {
+    VIEW_CHILDREN
+    READ_GLOBAL_CONTENT
+    READ_PRAYER
+    READ_GROUP
+  }
 
-    extend type HorizontalCardListFeature {
-        primaryAction: FeatureAction
-    }
+  extend type HorizontalCardListFeature {
+    primaryAction: FeatureAction
+  }
 
-    type ActionBarFeatureAction {
-        relatedNode: Node
-        action: ACTION_FEATURE_ACTION
-        title: String
+  type ActionBarFeatureAction {
+    relatedNode: Node
+    action: ACTION_FEATURE_ACTION
+    title: String
 
-        icon: String
-        theme: Theme
-    }
+    icon: String
+    theme: Theme
+  }
 
-    type ActionBarFeature implements Feature & Node {
-        id: ID!
-        order: Int
-    
-        actions: [ActionBarFeatureAction]
-    }
+  type ActionBarFeature implements Feature & Node {
+    id: ID!
+    order: Int
 
-    type AvatarListFeature implements Feature & Node {
-        id: ID!
-        order: Int
-    
-        people: [Person]
-        isCard: Boolean
-        primaryAction: ActionBarFeatureAction
-    }
+    actions: [ActionBarFeatureAction]
+  }
 
-    type LiveStreamListFeature implements Feature & Node {
-        id: ID!
-        order: Int
-        title: String
-        subtitle: String
-        liveStreams: [LiveStream]
-    }
-    
-    extend type Query {
-        connectFeedFeatures: [Feature] @cacheControl(maxAge: 0)
-        eventsFeedFeatures: [Feature] @cacheControl(maxAge: 0)
-        giveFeedFeatures: [Feature] @cacheControl(maxAge: 0)
-        userHeaderFeatures: [Feature] @cacheControl(maxAge: 0)
-    }
-`
+  type LiveStreamAction {
+    relatedNode: Node
+    action: ACTION_FEATURE_ACTION
+    title: String
+
+    start: Int
+    duration: Int
+  }
+
+  type AvatarListFeature implements Feature & Node {
+    id: ID!
+    order: Int
+
+    people: [Person]
+    isCard: Boolean
+    primaryAction: ActionBarFeatureAction
+  }
+
+  type LiveStreamListFeature implements Feature & Node {
+    id: ID!
+    order: Int
+    title: String
+    subtitle: String
+    liveStreams: [LiveStream]
+  }
+
+  extend type Query {
+    connectFeedFeatures: [Feature] @cacheControl(maxAge: 0)
+    eventsFeedFeatures: [Feature] @cacheControl(maxAge: 0)
+    giveFeedFeatures: [Feature] @cacheControl(maxAge: 0)
+    userHeaderFeatures: [Feature] @cacheControl(maxAge: 0)
+  }
+`;
