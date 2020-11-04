ce0ce001092f3c34ab25f4a8aaf2e6b83b6721c2
Jordan Wade <jjwade31@gmail.com>
2020-11-04 09:34:00 -0600

    Fix syntax error

--- src/data/event-content-item/resolver.js
+++ src/data/event-content-item/resolver.js
@@ -1,21 +1,12 @@
-import {
-  ContentItem as coreContentItem,
-} from '@apollosproject/data-connector-rock'
-import {
-  get,
-  flatten,
-  uniq,
-  uniqBy,
-  first,
-  filter
-} from 'lodash'
-import moment from 'moment'
-import momentTz from 'moment-timezone'
+import { ContentItem as coreContentItem } from '@apollosproject/data-connector-rock';
+import { get, flatten, uniq, uniqBy, first, filter } from 'lodash';
+import moment from 'moment';
+import momentTz from 'moment-timezone';
 
-import { parseRockKeyValuePairs } from '../utils'
-import sanitizeHtml from '../sanitize-html'
-import { sharingResolver } from '../content-item/resolver'
-import deprecatedResolvers from './deprecated-resolvers'
+import { parseRockKeyValuePairs } from '../utils';
+import sanitizeHtml from '../sanitize-html';
+import { sharingResolver } from '../content-item/resolver';
+import deprecatedResolvers from './deprecated-resolvers';
 
 const resolver = {
   EventContentItem: {
@@ -119,32 +110,35 @@ const resolver = {
         const schedule = get(item, 'attributeValues.schedule.value', '');
         const filters = get(item, 'attributeValues.filters.value', '');
 
-        if (schedule && schedule !== "" && filters && filters !== "") {
-          filters.split(',').forEach(filter => {
+        if (schedule && schedule !== '' && filters && filters !== '') {
+          filters.split(',').forEach((filter) => {
             if (filterScheduleDictionary[filter]) {
-              filterScheduleDictionary[filter].push(schedule)
+              filterScheduleDictionary[filter].push(schedule);
             } else {
-              filterScheduleDictionary[filter] = [schedule]
+              filterScheduleDictionary[filter] = [schedule];
             }
-          })
+          });
         }
-      })
-
-      return Object.entries(filterScheduleDictionary).map(([name, schedules]) => {
-        return {
-          name,
-          instances: async () => {
-            const rockSchedules = await Schedule.getFromIds(schedules)
-            const times = await Promise.all(rockSchedules.map(s => Event.parseScheduleAsEvents(s)))
-
-            return uniqBy(
-              flatten(times)
-                .sort((a, b) => moment(a.start).diff(b.start)),
-              'start'
-            )
-          }
-        });
       });
+
+      return Object.entries(filterScheduleDictionary).map(
+        ([name, schedules]) => {
+          return {
+            name,
+            instances: async () => {
+              const rockSchedules = await Schedule.getFromIds(schedules);
+              const times = await Promise.all(
+                rockSchedules.map((s) => Event.parseScheduleAsEvents(s))
+              );
+
+              return uniqBy(
+                flatten(times).sort((a, b) => moment(a.start).diff(b.start)),
+                'start'
+              );
+            },
+          };
+        }
+      );
 
       return Object.entries(filterScheduleDictionary).map(
         ([name, schedules]) => {
