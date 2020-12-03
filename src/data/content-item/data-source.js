6cad746d110b20707d93bf638fd988a7dbcbd084
Caleb Panza <caleb.panza@icloud.org>
2020-12-03 10:53:28 -0500

    New byAttributeValue method on Content Item

--- src/data/content-item/data-source.js
+++ src/data/content-item/data-source.js
@@ -144,6 +144,13 @@ export default class ContentItem extends coreContentItem.dataSource {
     return contentItem;
   };
 
+  byAttributeValue = async (key, value) => {
+    const requestBase = 'ContentChannelItems/GetByAttributeValue';
+    return this.request(
+      `${requestBase}?attributeKey=${key}&value=${value}&loadAttributes=expanded`
+    );
+  };
+
   attributeIsRedirect = ({ key, attributeValues, attributes }) =>
     key.toLowerCase().includes('redirect') &&
     typeof attributeValues[key].value === 'string' &&
