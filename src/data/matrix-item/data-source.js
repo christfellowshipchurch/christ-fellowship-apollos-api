7f181439cedc2c90de8aadb20e075daf222674f3
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2020-12-05 09:37:24 -0500

    Increase load part 1 (#181)
    * Cache attribute values
    
    * Point API to primary Rock server
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

--- src/data/matrix-item/data-source.js
+++ src/data/matrix-item/data-source.js
@@ -1,15 +1,37 @@
 import RockApolloDataSource from '@apollosproject/rock-apollo-data-source';
 
-import { getIdentifierType } from '../utils'
+import { getIdentifierType } from '../utils';
 
 export default class MatrixItem extends RockApolloDataSource {
-    expanded = true;
+  expanded = true;
 
-    getItemsFromId = async (id) =>
-        id
-            ? this.request('/AttributeMatrixItems')
-                .filter(`AttributeMatrix/${getIdentifierType(id).query}`)
-                .orderBy("Order")
-                .get()
-            : [];
+  getItemsFromId = async (id) => {
+    const { Cache } = this.context.dataSources;
+    const cachedKey = `attribute_matrix_${id}`;
+    const cachedValue = await Cache.get({
+      key: cachedKey,
+    });
+
+    if (cachedValue) {
+      return cachedValue;
+    }
+
+    return id
+      ? this.request('/AttributeMatrixItems')
+          .filter(`AttributeMatrix/${getIdentifierType(id).query}`)
+          .orderBy('Order')
+          .transform((results) => {
+            if (results) {
+              Cache.set({
+                key: cachedKey,
+                data: results,
+                expiresIn: 60 * 60, // 1 hour cache
+              });
+            }
+
+            return results;
+          })
+          .get()
+      : [];
+  };
 }
