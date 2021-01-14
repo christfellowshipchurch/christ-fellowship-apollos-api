3f998fc084e8211bd2e70f989e3486ad59336f10
Ryan Davidson <ryan@differential.com>
2021-01-14 15:42:50 -0500

    Delete old Search module for content items

--- src/data/search/index.js
+++
@@ -1,4 +0,0 @@
-export { default as dataSource } from './data-source';
-export { default as jobs } from './jobs';
-export { default as resolver } from './resolver';
-export { default as schema } from './schema';
