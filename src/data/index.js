24cfcdcd3ffe044c3857fd3f2e7508155d0a7b09
Jordan Wade <jjwade31@gmail.com>
2021-01-29 16:48:47 -0600

    Revert "Add group finder preferences and sub-preferences (#201)"
    This reverts commit 92399c8061bb10431bc8744d84894e13a8e26230.

--- src/data/index.js
+++ src/data/index.js
@@ -49,7 +49,6 @@ import * as PageBuilder from './page-builder';
 import * as Person from './people';
 import * as PhoneNumber from './phone-number';
 import * as PrayerRequest from './prayer-request';
-import * as Preferences from './preferences';
 import * as RockConstants from './rock-constants';
 import * as Schedule from './schedule';
 import * as Search from './search';
@@ -102,7 +101,6 @@ const data = {
   PersonalDevice,
   PhoneNumber,
   PrayerRequest,
-  Preferences,
   RockConstants,
   Schedule,
   Scripture,
--- src/data/preferences/index.js
+++
@@ -1,3 +0,0 @@
-export { default as resolver } from './resolver'
-export { default as schema } from './schema'
-export { default as dataSource } from './data-source'
