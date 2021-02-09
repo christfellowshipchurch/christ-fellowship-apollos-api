9abe09f0b9495479caa678c36f6d37d450e97c11
Ryan Davidson <ryan@differential.com>
2021-02-09 11:01:25 -0500

    Delete unused redis-queue-options

--- src/data/search/index.js
+++ src/data/search/index.js
@@ -1,4 +1,3 @@
 export { default as dataSource } from './data-source';
-export { default as redisQueueOptions } from './redis-queue-options';
 export { default as resolver, searchResultResolvers } from './resolver';
 export { default as schema } from './schema';
