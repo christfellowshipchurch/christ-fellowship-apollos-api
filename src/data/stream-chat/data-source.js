8bd2c7ffb0fe0c2730e591980573574a5876ea34
Caleb Panza <caleb.panza@icloud.org>
2021-04-06 12:04:35 -0400

    Quick patch

--- src/data/stream-chat/data-source.js
+++ src/data/stream-chat/data-source.js
@@ -233,13 +233,17 @@ export default class StreamChat extends RESTDataSource {
     const members = get(data, 'members', []);
     const memberIds = members
       .filter(({ user }) => !user.banned) // user isn't banned
-      .filter(({ user }) => !user.online) // user isn't currently online
+      .filter(({ banned }) => !banned) // user isn't banned from the channel
+      .filter(({ user }) => !user.shadow_banned) // user isn't shadow banned from the channel
+      // .filter(({ user }) => !user.online) // user isn't currently online
       .map(({ user_id }) => user_id)
       .filter((id) => id !== sender.id);
 
     if (channelId && channelType) {
       const channel = await this.getChannel({ channelId, channelType });
-      const mutedUsers = get(channel, 'channel.muteNotifications', []);
+      const mutedNotifications = get(channel, 'channel.muteNotifications', []);
+      const mutedUsers =
+        mutedNotifications && Array.isArray(mutedNotifications) ? mutedNotifications : [];
 
       const rockAliasIds = await Promise.all(
         memberIds
@@ -247,6 +251,7 @@ export default class StreamChat extends RESTDataSource {
           .map(async (id) => {
             const { id: rockPersonId } = parseGlobalId(`Person:${id}`);
             const person = await Person.getFromId(rockPersonId);
+
             return get(person, 'primaryAliasId');
           })
       );
