f21c65bc9b54fa1628781c2c879efeedc3a54a05
Caleb Panza <caleb.panza@icloud.org>
2021-04-08 17:43:03 -0400

    Code Clean Up

--- src/data/stream-chat/server-middleware.js
+++ src/data/stream-chat/server-middleware.js
@@ -1,29 +1,5 @@
 import { get } from 'lodash';
 
-async function handleNewMessage(data, { Person, OneSignal }) {
-  const senderName = get(data, 'user.name');
-  const memberIds = get(data, 'members', []).map(({ user_id }) => user_id);
-  const content = get(data, 'message.text', '');
-
-  console.log({ context });
-
-  const aliasIds = await Promise.all(
-    memberIds.map(async (id) => {
-      const { id: rockPersonId } = parseGlobalId(`Person:${id}`);
-      const person = await Person.getFromId(rockPersonId);
-      return get(person, 'primaryAliasId');
-    })
-  );
-
-  console.log({ aliasIds });
-
-  // OneSignal.createNotification({
-  //   toUserIds = aliasIds,
-  //   content,
-  //   heading: `New Message from ${senderName}`,
-  // })
-}
-
 export default ({ app, getContext }) => {
   app.post('/stream-chat/webhook', (req, res) => {
     const context = getContext({ req });
