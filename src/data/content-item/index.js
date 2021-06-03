638b84f5ae21b99b0c8d6451bd60928b7931e9dd
Ryan Davidson <ryanrkd2112@gmail.com>
2021-06-03 14:13:42 -0400

    Fixed jobs for content and groups

--- src/data/content-item/index.js
+++ src/data/content-item/index.js
@@ -1,3 +1,4 @@
-export { default as dataSource } from './data-source'
-export { default as resolver } from './resolver'
-export { default as schema } from './schema'
+export { default as dataSource } from './data-source';
+export { default as resolver } from './resolver';
+export { default as schema } from './schema';
+export { default as jobs } from './jobs';
